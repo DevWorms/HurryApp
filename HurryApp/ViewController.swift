cdeab9078579dbbe9e495030e1dbb717b6bcfcea
Valentin Granados <valefcfhc@gmail.com>
2015-11-22 14:54:18 -0600

    Initial Commit

---
+++ HurryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HurryApp
+//
+//  Created by Emmanuel Valentín Granados López on 22/11/15.
+//  Copyright © 2015 DevWorms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
