25628722a64022faebe519e6e302c15cb5a0e94b
Valentin Granados <valefcfhc@gmail.com>
2015-11-23 00:42:37 -0600

    tableView

---
+++ HurryApp/ComprarViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ComprarViewController.swift
+//  HurryApp
+//
+//  Created by Emmanuel Valentín Granados López on 23/11/15.
+//  Copyright © 2015 DevWorms. All rights reserved.
+//
+
+import UIKit
+
+class ComprarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UITableViewDelegate, UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let stringIndex = String( indexPath.row )
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier( stringIndex ) as UITableViewCell!
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
